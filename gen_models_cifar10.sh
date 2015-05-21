01f343d817114865de4423a9dfcd553bbe8aafd4
Jin Hwa Kim <jnhwkim@gmail.com>
2015-05-21 17:15:15 +0900

    rename the extension of templates.

--- gen_models_cifar10.sh
+++ gen_models_cifar10.sh
@@ -7,7 +7,7 @@ do
 		echo "$col1 *** $col2 *** $col3 *** $col4"
 		continue
 	else
-		sed -e "s;%Idx%;$col1;g" -e "s;%Start%;$col2;g" -e "s;%End%;$col3;g" -e "s;%LR%;$col4;g" templates/cifar10_full_solver.prototxt > models/cifar10/cifar10_full_solver_$col1.prototxt
-		sed -e "s;%Idx%;$col1;g" -e "s;%Start%;$col2;g" -e "s;%End%;$col3;g" -e "s;%LR%;$col4;g" templates/cifar10_full_train_test.prototxt > models/cifar10/cifar10_full_train_test_$col1.prototxt
+		sed -e "s;%Idx%;$col1;g" -e "s;%Start%;$col2;g" -e "s;%End%;$col3;g" -e "s;%LR%;$col4;g" templates/cifar10_full_solver.txt > models/cifar10/cifar10_full_solver_$col1.prototxt
+		sed -e "s;%Idx%;$col1;g" -e "s;%Start%;$col2;g" -e "s;%End%;$col3;g" -e "s;%LR%;$col4;g" templates/cifar10_full_train_test.txt > models/cifar10/cifar10_full_train_test_$col1.prototxt
 	fi
 done < src/vernu121-1.csv
